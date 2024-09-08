bc2673e631c9abe325a8e6d549030ae046309bfd
Rebecca Pösche Ruud <rebecca.ruud@norsk-tipping.no>
2024-09-08 17:30:51 +0200

    Update test

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,6 +9,7 @@ const AboutMe = () => {
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-gray-700">
               About
+
             </p>
           </div>
         </div>
